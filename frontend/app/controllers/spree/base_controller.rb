e7883227881e02bbcb36a4c8436fc41d41306d93
Ryan Bigg <radarlistener@gmail.com>
2012-11-14 09:57:29 +1100

    Re-add RespondWith module

--- frontend/app/controllers/spree/base_controller.rb
+++ frontend/app/controllers/spree/base_controller.rb
@@ -3,4 +3,5 @@ require 'cancan'
 class Spree::BaseController < ApplicationController
   include Spree::Core::ControllerHelpers::Auth
   include Spree::Core::ControllerHelpers::Common
+  include Spree::Core::ControllerHelpers::RespondWith
 end
